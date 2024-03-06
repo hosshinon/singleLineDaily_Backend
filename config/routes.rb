703f8bc2cd28ee5f50bd78eb0d2b82e9dc7f6c0a
hosshinon <155843587+hosshinon@users.noreply.github.com>
2024-03-06 20:26:13 +0900

    origin削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resources :posts
     end
   end
-  match '*path' => 'options_request#response_preflight_request', via: :options
+  # match '*path' => 'options_request#response_preflight_request', via: :options
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
